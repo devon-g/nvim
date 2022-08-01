2b498b29f52ca52b4d74fbebf0d020a30e7bab8d
Devon Gardner <dgardner365@gmail.com>
2022-07-31 22:33:44 -0400

    merged dap configs into one file

--- lua/nvim_dap_virtual_text.lua
+++
@@ -1,9 +0,0 @@
-local nvim_dap_virtual_text_ok, nvim_dap_virtual_text = pcall(require, 'nvim-dap-virtual-text')
-if not nvim_dap_virtual_text_ok then
-  print('nvim-dap-virtual-text failed to load.')
-  return
-end
-
-nvim_dap_virtual_text.setup({
-  commented = true
-})
