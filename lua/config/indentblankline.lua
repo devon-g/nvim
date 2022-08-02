d498eb516fde803fdf2cbc12079b0cd4aa8c2fcc
Devon Gardner <dgardner365@gmail.com>
2022-08-02 13:14:35 -0400

    configured indentblankline characters and context highlighting

---
+++ lua/config/indentblankline.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+function M.setup()
+  vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
+  vim.g.indent_blankline_char_list = {"|", "¦", "┆", "┊"}
+  vim.g.indent_blankline_filetype_exclude = { "help", "packer" }
+  vim.g.indent_blankline_show_trailing_blankline_indent = false
+  vim.g.indent_blankline_show_current_context = true
+end
+
+return M
