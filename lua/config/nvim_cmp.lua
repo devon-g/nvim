34c3e59c33b4b63a3c80416bec7daf4481181251
Devon Gardner <dgardner365@gmail.com>
2022-08-07 01:43:30 -0400

    reconfigured keybinds and added lsp support

--- lua/config/nvim_cmp.lua
+++ lua/config/nvim_cmp.lua
@@ -8,23 +8,16 @@ function M.setup()
     return
   end
 
-  local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-  end
-
-  local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-  end
+  local utils = require("utils")
 
   cmp.setup({
-    mapping = {
+    mapping = cmp.mapping.preset.insert({
       ["<C-n>"] = cmp.mapping({
         c = function()
           if cmp.visible() then
             cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
           else
-            vim.api.nvim_feedkeys(t("<Down>"), "n", true)
+            vim.api.nvim_feedkeys(utils.t("<Down>"), "n", true)
           end
         end,
         i = function(fallback)
@@ -40,7 +33,7 @@ function M.setup()
           if cmp.visible() then
             cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
           else
-            vim.api.nvim_feedkeys(t("<Up>"), "n", true)
+            vim.api.nvim_feedkeys(utils.t("<Up>"), "n", true)
           end
         end,
         i = function(fallback)
@@ -51,21 +44,12 @@ function M.setup()
           end
         end
       }),
-      ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-      ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-      ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c", }),
-      ["<C-e>"] = cmp.mapping({ i = cmp.mapping.close(), c = cmp.mapping.close() }),
-      ["<CR>"] = cmp.mapping({
-        i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
-        c = function(fallback)
-          if cmp.visible() then
-            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-          else
-            fallback()
-          end
-        end,
-      }),
-    },
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    }),
     snippet = {
       expand = function(args)
         luasnip.lsp_expand(args.body)
@@ -75,6 +59,7 @@ function M.setup()
     formatting = {
       format = function(entry, vim_item)
         vim_item.menu = ({
+          nvim_lsp = "[LSP]",
           buffer = "[Buffer]",
           luasnip = "[Snip]",
           nvim_lua = "[Lua]",
@@ -83,22 +68,40 @@ function M.setup()
         return vim_item
       end,
     },
-    sources = {
-      { name = "treesitter" },
-      { name = "buffer" },
-      { name = "luasnip" },
+    sources = cmp.config.sources({
       { name = "nvim_lua" },
+      { name = "nvim_lsp" },
+      -- { name = "treesitter" },
+      { name = "luasnip" },
       { name = "path" },
       { name = "spell" },
       { name = "emoji" },
       { name = "calc" },
-    },
+    }, {
+      { name = "buffer" },
+    }),
     window = {
-      documentation = {
-        border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-        winhighlight = "NormalFloat:NormalFloat,FloatBorder:TelescopeBorder",
-      },
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
     },
+  })
+
+  -- Use buffer sources for `/`
+  cmp.setup.cmdline("/", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = "buffer" },
+    },
+  })
+
+  -- Use cmdline & path source for ":"
+  cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = "path" },
+    }, {
+      { name = "cmdline" },
+    })
   })
 
   cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
