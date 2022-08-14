28a791ade42c46022dc67829c7c61c5ecb9bb2c3
Devon Gardner <dgardner365@gmail.com>
2022-08-14 02:59:10 -0400

    removed navic

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,23 +1,12 @@
 local M = {}
 
 function M.setup()
-  local navic = require("nvim-navic")
-
   require("lualine").setup({
     options = {
       disabled_filetypes = {},
       component_separators = "",
       section_separators = "",
       globalstatus = true,
-    },
-    sections = {
-      lualine_c = {
-        { 'filename' },
-        {
-          navic.get_location,
-          cond = navic.is_available,
-        },
-      },
     },
   })
 end
