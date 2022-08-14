96eb6f2e90a279027d422c2bbaf8353b6281d9a5
Devon Gardner <dgardner365@gmail.com>
2022-08-13 22:25:07 -0400

    removed lazygit config for toggleterm, replaced with lazygit plugin

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -12,26 +12,6 @@ function M.setup()
 
   -- Configure toggleterm global settings
   toggleterm.setup(config)
-
-  -- Custom terminal for lazygit
-  local Terminal = require("toggleterm.terminal").Terminal
-  local lazygit = Terminal:new({
-    cmd = "lazygit",
-    dir = "git_dir",
-    float_opts = {
-      border = "curved",
-    },
-    on_open = function(term)
-      vim.cmd("startinsert!")
-      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-    end,
-  })
-
-  -- Create global function to toggle the lazygit terminal
-  -- Mapped to <leader>g in after/plugin/keymaps.lua
-  function _lazygit_toggle()
-    lazygit:toggle()
-  end
 end
 
 return M
