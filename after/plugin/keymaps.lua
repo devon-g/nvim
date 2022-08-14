dd42f0d54e9f2d92fd88167488a72098f43ea598
Devon Gardner <dgardner365@gmail.com>
2022-08-13 22:24:30 -0400

    changed lazygit terminal keybind

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -22,7 +22,7 @@ vim.api.nvim_set_keymap("n", "<Up>", ":resize -1<CR>", opts)
 vim.api.nvim_set_keymap("n", "<Down>", ":resize +1<CR>", opts)
 
 -- Open lazygit popup terminal
-vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>LazyGit<CR>", opts)
 
 -- Open fuzzy finder
 vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>FzfLua files<CR>", opts)
