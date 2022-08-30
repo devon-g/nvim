c0c2ce2683f932cd0a596f86ce157af267c62e58
Devon Gardner <dgardner365@gmail.com>
2022-08-30 13:43:27 -0400

    commented out some telescope extensions that are still in testing

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -5,9 +5,9 @@ function M.setup()
 
   telescope.setup()
   telescope.load_extension("fzf")
-  telescope.load_extension("project") -- telescope-project.nvim
-  telescope.load_extension("repo")
-  telescope.load_extension("file_browser")
+  -- telescope.load_extension("project") -- telescope-project.nvim
+  -- telescope.load_extension("repo")
+  -- telescope.load_extension("file_browser")
   telescope.load_extension("projects") -- project.nvim
 end
 
