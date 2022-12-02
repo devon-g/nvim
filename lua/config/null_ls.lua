90c9cfc5f8aa33b903fb9ee6adb800f61689166c
Devon Gardner <dgardner365@gmail.com>
2022-12-02 17:01:07 -0500

    set up c++ and cmake formatters

--- lua/config/null_ls.lua
+++ lua/config/null_ls.lua
@@ -1,7 +1,20 @@
 local M = {}
 
 function M.setup()
+  local null_ls = require("null-ls")
+  local f = null_ls.builtins.formatting
+  local d = null_ls.builtins.diagnostics
+  null_ls.setup({
+    sources = {
+      -- C++
+      f.clang_format,
+      d.clang_check,
 
+      -- CMake
+      f.cmake_format,
+      d.cmake_lint,
+    }
+  })
 end
 
 return M
