707784cc101be7341fb3d8b06614024bba5e7db3
Devon Gardner <dgardner365@gmail.com>
2023-04-04 22:53:46 -0400

    starting over from scratch with lazy.nvim plugin manager :^)

---
+++ plugin/options.lua
@@ -0,0 +1,12 @@
+vim.o.background = "dark"
+vim.o.colorcolumn = "80,120"
+vim.o.expandtab = true
+vim.o.ignorecase = true
+vim.o.number = true
+vim.o.shiftwidth = 4
+vim.o.smartcase = true
+vim.o.smartindent = true
+vim.o.tabstop = 4
+vim.o.termguicolors = true
+vim.o.undofile = true
+vim.o.wrap = false
