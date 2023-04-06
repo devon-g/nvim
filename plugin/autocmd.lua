f4089c2b0cfbfe61cacf9a98df2174718925f3da
Devon Gardner <dgardner365@gmail.com>
2023-04-06 16:41:09 -0400

    reconfigured the way my lsp keymaps are set

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -2,19 +2,44 @@
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("UserLspConfig", {}),
     callback = function(ev)
-        -- Create mappings for local buffers
-        local opts = { buffer = ev.buf }
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "<space>f", function()
+        -- Helper function to make adding lsp keymaps simpler
+        local nmap = function (keys, func, desc)
+            if desc then
+                desc = "LSP: " .. desc
+            end
+
+            vim.keymap.set("n", keys, func, { buffer = ev.buf, desc = desc })
+        end
+
+        -- Attach useful lsp functions to keybinds
+        -- Movement
+        nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+        nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+        nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+        nmap("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
+
+        -- Documentation
+        nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+        nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+        nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+        -- Actions
+        nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+        nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+        nmap("<space>rf", function ()
             vim.lsp.buf.format({ async = true })
-        end, opts)
+        end, "[R]e[f]ormat")
+
+        -- Workspaces
+        nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+        nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+        nmap("<leader>wl", function ()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, "[W]orkspace [L]ist Folders")
+
+        -- Find symbols
+        nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+        nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
     end,
 })
