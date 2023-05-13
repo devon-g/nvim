7be7c882b5662580c2dee608dc1fae6fa9b061a5
Devon Gardner <dgardner365@gmail.com>
2023-05-13 17:20:38 -0400

    changed some buffer options for latex

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -1,3 +1,5 @@
+vim.o.shiftwidth = 8
+vim.o.softtabstop = 8
 vim.o.spell = true
 vim.o.spelllang = "en_us"
-vim.o.textwidth = 80
+vim.o.textwidth = 79
--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -1,13 +1,10 @@
-vim.o.background = "dark"
-vim.o.colorcolumn = "80,120"
-vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.colorcolumn = "80"
 vim.o.ignorecase = true
---vim.o.number = true
+vim.o.number = true
 vim.o.shiftwidth = 4
 vim.o.smartcase = true
-vim.o.smartindent = true
-vim.o.softtabstop = 4
-vim.o.tabstop = 4
+vim.o.softtabstop = 4 -- negative value means use same as shiftwidth
 vim.o.termguicolors = true
 vim.o.undofile = true
 vim.o.wrap = false
