886da722b0c62dade25a5d0a2af007491ab420de
Devon Gardner <dgardner365@gmail.com>
2023-07-02 21:19:24 -0400

    Added gitsigns for line blame and per line status

---
+++ lua/plugins/git.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = "nvim-lua/plenary.nvim"
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup()
+    end,
+  }
+}
