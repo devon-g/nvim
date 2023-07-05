b31ef167389188ddd643b24dbe93882e870e508f
Devon Gardner <dgardner365@gmail.com>
2023-07-04 23:47:57 -0400

    Using telescope to find executables for dap

--- lua/plugins/dap/configs.lua
+++ lua/plugins/dap/configs.lua
@@ -1,3 +1,9 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
 return {
   setup = function (dap)
     dap.configurations.python = {
@@ -17,8 +23,22 @@ return {
 	type = "codelldb",
 	request = "launch",
 	name = "Launch",
-	program = function ()
-	  return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. '/', "file")
+	program = function()
+	  return coroutine.create(function(coro)
+	    local opts = {}
+	    pickers.new(opts, {
+	      prompt_title = "Path to executable",
+	      finder = finders.new_oneshot_job({ "fd", "--hidden", "--no-ignore", "--type", "x" }, {}),
+	      sorter = conf.generic_sorter(opts),
+	      attach_mappings = function(buffer_number)
+		actions.select_default:replace(function()
+		  actions.close(buffer_number)
+		  coroutine.resume(coro, action_state.get_selected_entry()[1])
+		end)
+		return true
+	      end,
+	    }):find()
+	  end)
 	end,
 	cwd = "${workspaceFolder}",
 	stopOnEntry = false,
