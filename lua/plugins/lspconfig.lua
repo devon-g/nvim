7a5568e211151a464aa84b7a6410b9f8dee7e00b
Devon Gardner <dgardner365@gmail.com>
2023-07-05 23:14:54 -0400

    modified nvim-cmp binds; better signature support

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -37,12 +37,15 @@ return {
 
       -- Manage lsp servers
       local mason_lspconfig = require("mason-lspconfig")
-      local lspconfig       = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
       -- Update client capabilities to include what nvim-cmp supports
-      local capabilities    = vim.lsp.protocol.make_client_capabilities()
-      capabilities          = require("cmp_nvim_lsp").default_capabilities(capabilities)
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
       capabilities.textDocument.completion.completionItem.snippetSupport = true
+      capabilities.textDocument.completion.completionItem.resolveSupport = {
+	properties = { "documentation", "detail", "additionalTextEdits" },
+      }
 
       -- Set up neovim custom lua support
       require("neodev").setup({
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -3,7 +3,6 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lsp-signature-help",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
@@ -12,33 +11,40 @@ return {
       "L3MON4D3/LuaSnip",
 
       "windwp/nvim-autopairs",
+      "ray-x/lsp_signature.nvim",
     },
     config = function()
       local cmp = require("cmp")
       local luasnip = require("luasnip")
       require("luasnip.loaders.from_vscode").lazy_load()
+      require("lsp_signature").setup()
 
       cmp.setup({
         snippet = {
-          expand = function(args)
+          expand = function (args)
             luasnip.lsp_expand(args.body)
           end,
         },
         mapping = cmp.mapping.preset.insert({
+	  ["<C-p>"] = cmp.mapping.select_prev_item(),
+	  ["<C-n>"] = cmp.mapping.select_next_item(),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = false }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if luasnip.expand_or_jumpable() then
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function (fallback)
+	    if cmp.visible() then
+	      cmp.select_next_item()
+	    elseif luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
             else
               fallback()
             end
           end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if luasnip.jumpable(-1) then
+          ["<S-Tab>"] = cmp.mapping(function (fallback)
+	    if cmp.visible() then
+	      cmp.select_prev_item()
+	    elseif luasnip.jumpable(-1) then
               luasnip.jump(-1)
             else
               fallback()
@@ -48,11 +54,12 @@ return {
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" },
-          { name = "nvim_lsp_signature_help" },
           { name = "path" },
         }, {
           { name = "buffer" },
-        })
+        }),
+	completion = { completeopt = "menu,menuone,noinsert" },
+	experimental = { ghost_text = true },
       })
 
       local cmp_autopairs = require("nvim-autopairs.completion.cmp")
