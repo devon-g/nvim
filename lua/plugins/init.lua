146164c6e344756fe90d2173e7b4eb1d556aed84
Devon Gardner <dgardner365@gmail.com>
2023-12-21 14:13:19 -0500

    Removed some misc plugins gave rest own conf file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,23 +1,3 @@
 return {
   "folke/lazy.nvim",
-  {
-    "ellisonleao/gruvbox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.api.nvim_command("colorscheme gruvbox")
-    end,
-  },
-  {
-    "numToStr/Comment.nvim",
-    config = true,
-  },
-  {
-    "windwp/nvim-autopairs",
-    config = true,
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    config = true,
-  }
 }
