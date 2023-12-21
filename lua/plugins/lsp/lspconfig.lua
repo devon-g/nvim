4818f663f7289db363036da1e8f1f9bcdb634dc2
Devon Gardner <dgardner365@gmail.com>
2023-12-21 01:14:36 -0500

    reorganized lsp configs

---
+++ lua/plugins/lsp/lspconfig.lua
@@ -0,0 +1,67 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    { "antosha417/nvim-lsp-file-operations", config = true },
+    "folke/neodev.nvim"
+  },
+  config = function ()
+    local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local telescope_builtin = require("telescope.builtin")
+
+
+    local on_attach = function (_, bufnr)
+      local opts = { buffer = bufnr, noremap = true, silent = true }
+      opts.desc = "Show LSP references"
+      vim.keymap.set("n", "gR", telescope_builtin.lsp_references, opts)
+
+      opts.desc = "Go to declaration"
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+      opts.desc = "Show LSP definitions"
+      vim.keymap.set("n", "gd", telescope_builtin.lsp_definitions, opts)
+
+      opts.desc = "Show LSP implementations"
+      vim.keymap.set("n", "gi", telescope_builtin.lsp_implementations, opts)
+
+      opts.desc = "Show LSP type definitions"
+      vim.keymap.set("n", "gt", telescope_builtin.lsp_type_definitions, opts)
+
+      opts.desc = "See available code actions"
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+      opts.desc = "Smart rename"
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+      opts.desc = "Show buffer diagnostics"
+      vim.keymap.set("n", "<leader>D", telescope_builtin.diagnostics, opts)
+
+      opts.desc = "Show line diagnostics"
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+      opts.desc = "Go to previous diagnostic"
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+
+      opts.desc = "Go to next diagnostic"
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+      opts.desc = "Show documentation for what is under cursor"
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    end
+
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+
+    require("neodev").setup()
+
+    lspconfig["pylsp"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+    lspconfig["lua_ls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+  end,
+}
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,26 +1,28 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-
-      "SirVer/ultisnips",
-      "quangnguyen30192/cmp-nvim-ultisnips",
-
-      "windwp/nvim-autopairs",
-      "ray-x/lsp_signature.nvim",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
     },
     config = function()
       local cmp = require("cmp")
-      require("lsp_signature").setup()
+      local luasnip = require("luasnip")
+
+      require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
+	completion = {
+	  completeopt = "menu,menuone,preview,noselect",
+	},
         snippet = {
           expand = function (args)
-	    vim.fn["UltiSnips#Anon"](args.body)
+	    luasnip.lsp_expand(args.body)
           end,
         },
         mapping = cmp.mapping.preset.insert({
@@ -28,32 +30,16 @@ return {
 	  ["<C-n>"] = cmp.mapping.select_next_item(),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
+	  ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function (fallback)
-	    if cmp.visible() then
-	      cmp.select_next_item()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function (fallback)
-	    if cmp.visible() then
-	      cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "ultisnips" },
+          { name = "luasnip" },
+	  { name = "buffer" },
           { name = "path" },
-        }, {
-          { name = "buffer" },
         }),
-	completion = { completeopt = "menu,menuone,noinsert" },
-	experimental = { ghost_text = true },
       })
 
       local cmp_autopairs = require("nvim-autopairs.completion.cmp")
