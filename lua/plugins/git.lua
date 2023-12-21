fc99de6455cf1883ab47519c48709c66340aed9a
Devon Gardner <dgardner365@gmail.com>
2023-12-21 01:23:38 -0500

    Added optional neogit dependencies

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,10 +1,15 @@
 return {
   {
     "NeogitOrg/neogit",
-    dependencies = "nvim-lua/plenary.nvim"
+    dependencies = {
+	"nvim-lua/plenary.nvim",
+	"sindrets/diffview.nvim",
+	"nvim-telescope/telescope.nvim",
+    },
+    config = true,
   },
   {
     "lewis6991/gitsigns.nvim",
-    config = true
+    config = true,
   }
 }
