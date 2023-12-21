d028f0ab3785fa3da3f8ea1ab4243ad34c52a0d7
Devon Gardner <dgardner365@gmail.com>
2023-12-21 01:15:50 -0500

    moved telescope keybinds into config file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,23 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      "nvim-tree/nvim-web-devicons",
+    },
     config = function()
-      require("telescope").load_extension("fzf")
+      local telescope = require("telescope")
+      local telescope_builtin = require("telescope.builtin")
+
+      telescope.setup({})
+      telescope.load_extension("fzf")
+
+      -- Setup keymaps
+      vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, { desc = "Fuzzy find files in cwd" })
+      vim.keymap.set("n", "<leader>fr", telescope_builtin.oldfiles, { desc = "Fuzzy find recent files" })
+      vim.keymap.set("n", "<leader>fs", telescope_builtin.live_grep, { desc = "Find string in cwd" })
+      vim.keymap.set("n", "<leader>fc", telescope_builtin.grep_string, { desc = "Find string under cursor in cwd" })
     end
   },
-  {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    cond = function()
-      return vim.fn.executable("make") == 1
-    end,
-  }
 }
