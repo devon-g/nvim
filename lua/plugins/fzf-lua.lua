d8cac623d1b53b45dd8b0ef1698422aad7bfe941
Devon Gardner <dgardner365@gmail.com>
2023-12-26 21:52:38 -0500

    Added fzf keybinds

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -5,5 +5,11 @@ return {
       { "junegunn/fzf", build = "./install --bin" },
     },
     opts = {},
+    config = function()
+      local fzf_lua = require("fzf-lua")
+
+      vim.keymap.set("n", "<leader>ff", fzf_lua.files, { desc = "Fuzzy find files" })
+      vim.keymap.set("n", "<leader>fg", fzf_lua.live_grep, { desc = "Fuzzy find string" })
+    end,
   },
 }
