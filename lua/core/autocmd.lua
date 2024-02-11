b7536e5299534f4931568790aa584feda5435453
Devon Gardner <dgardner365@gmail.com>
2024-02-11 00:41:20 -0500

    Make nvim builtin terminals bearable

---
+++ lua/core/autocmd.lua
@@ -0,0 +1,12 @@
+local term_augroup = vim.api.nvim_create_augroup("TolerableTerm", {})
+-- Start terminal buffers in insert mode
+vim.api.nvim_create_autocmd({ "TermOpen", "WinEnter" }, {
+	group = term_augroup,
+	pattern = "term://*",
+	command = "startinsert",
+})
+-- Disable line numbers in terminal buffers
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = term_augroup,
+	command = "set nonumber norelativenumber"
+})
---
+++ lua/core/keymaps.lua
@@ -0,0 +1,3 @@
+-- Keymaps to make terminal mode a bit more bearable
+vim.keymap.set("t", [[<C-\>]], [[<C-\><C-n>]])
+vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]])
