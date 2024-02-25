4561c5ca2f1b48c04eb2271096a8888eb080465d
Devon Gardner <dgardner365@gmail.com>
2024-02-25 00:48:35 -0500

    Replaced neogit with fugitive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,13 +1,5 @@
 return {
-	{
-		"NeogitOrg/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"sindrets/diffview.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-		opts = {},
-	},
+	"tpope/vim-fugitive",
 	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPre", "BufNewFile" },
