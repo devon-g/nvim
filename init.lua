6a7908308a1fc60e835caab9790d33c8f1434329
Devon Gardner <devon@goosur.com>
2024-05-23 22:15:25 -0400

    bootstrapto mini.deps and migrate colorscheme

--- init.lua
+++ init.lua
@@ -1,26 +1,23 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+local path_package = vim.fn.stdpath("data") .. "/site"
+local mini_path = path_package .. '/pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/echasnovski/mini.nvim',
+    mini_path,
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
 end
-vim.opt.rtp:prepend(lazypath)
 
-require("core")
-require("lazy").setup("plugins", {
-  install = {
-    colorscheme = { "gruvbox" },
-  },
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
-})
+require('mini.deps').setup({ path = { package = path_package } })
+
+local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+
+now(function()
+  add('ellisonleao/gruvbox.nvim')
+  vim.cmd('colorscheme gruvbox')
+end)
