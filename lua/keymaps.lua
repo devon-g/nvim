14ff0bd17dc0ec3e179f2a939c35d77606f7873a
Devon Gardner <devon@goosur.com>
2024-05-31 20:41:47 -0400

    add keybind to populate loclist with todo comments

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps to make terminal mode a bit more bearable
 vim.keymap.set('t', [[<C-\>]], [[<C-\><C-n>]])
 vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
+
+-- Populate location list with TODO comments
+vim.keymap.set('n', '<leader>td', function()
+  vim.cmd('lvimgrep /TODO: /j **')
+  vim.cmd('lopen')
+end)
