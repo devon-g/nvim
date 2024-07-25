91398f1b15b950b44114a8d7b5bb179f2c7a69ac
Devon Gardner <devon@goosur.com>
2024-07-25 16:59:50 -0400

    removed conform and formatted mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -20,50 +20,50 @@ return {
     local lspconfig = require("lspconfig")
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
     mason_lspconfig.setup_handlers({
-	    -- Default handler
-	    function(server_name)
-		    lspconfig[server_name].setup({
-			    capabilities = capabilities,
-		    })
-	    end,
-	    -- Specific handlers
-	    ["lua_ls"] = function() -- Requires special setup for nvim configs
-		    require("neodev").setup({})
-		    lspconfig.lua_ls.setup({
-			    capabilities = capabilities,
-			    settings = {
-				    Lua = {
-					    workspace = { checkThirdParty = false },
-					    telemetry = { enable = false },
-				    },
-			    },
-		    })
-	    end,
-	    ["rust_analyzer"] = function()
-		    lspconfig.rust_analyzer.setup({
-			    settings = {
-				    ["rust-analyzer"] = {
-					    checkOnSave = {
-						    allTargets = false,
-					    },
-				    },
-			    },
-		    })
-	    end,
-	    ["jdtls"] = function()
-		    lspconfig.jdtls.setup({
-			    cmd = {
-				    "jdtls",
-				    "-configuration",
-				    tostring(vim.fn.getenv("HOME")) .. "/.cache/jdtls/config",
-				    "-data",
-				    tostring(vim.fn.getenv("HOME")) .. "/.cache/jdtls/workspace",
-				    "--jvm-arg=-javaagent:"
-					    .. require("mason-registry").get_package("jdtls"):get_install_path()
-					    .. "/lombok.jar",
-			    },
-		    })
-	    end,
+      -- Default handler
+      function(server_name)
+        lspconfig[server_name].setup({
+          capabilities = capabilities,
+        })
+      end,
+      -- Specific handlers
+      ["lua_ls"] = function() -- Requires special setup for nvim configs
+        require("neodev").setup({})
+        lspconfig.lua_ls.setup({
+          capabilities = capabilities,
+          settings = {
+            Lua = {
+              workspace = { checkThirdParty = false },
+              telemetry = { enable = false },
+            },
+          },
+        })
+      end,
+      ["rust_analyzer"] = function()
+        lspconfig.rust_analyzer.setup({
+          settings = {
+            ["rust-analyzer"] = {
+              checkOnSave = {
+                allTargets = false,
+              },
+            },
+          },
+        })
+      end,
+      ["jdtls"] = function()
+        lspconfig.jdtls.setup({
+          cmd = {
+            "jdtls",
+            "-configuration",
+            tostring(vim.fn.getenv("HOME")) .. "/.cache/jdtls/config",
+            "-data",
+            tostring(vim.fn.getenv("HOME")) .. "/.cache/jdtls/workspace",
+            "--jvm-arg=-javaagent:"
+              .. require("mason-registry").get_package("jdtls"):get_install_path()
+              .. "/lombok.jar",
+          },
+        })
+      end,
     })
 
     -- SET UP LSP KEYBINDS
@@ -105,9 +105,6 @@ return {
 
         opts.desc = "Smart rename"
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-
-        opts.desc = "Show documentation for what is under cursor"
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
       end,
     })
   end,
