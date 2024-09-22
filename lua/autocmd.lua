7f510d689d8a3a428424288ced7ec5abfb3657b3
Devon Gardner <devon@goosur.com>
2024-09-21 21:53:12 -0400

    Relocated lsp autocmd to autocmd file

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -10,3 +10,31 @@ vim.api.nvim_create_autocmd('TermOpen', {
   group = term_augroup,
   command = 'set nonumber norelativenumber',
 })
+
+local lsp_augroup = vim.api.nvim_create_augroup('UserLspConfig', {})
+-- Configure lsp to use MiniCompletion and set up some keymaps
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = lsp_augroup,
+  callback = function(args)
+    vim.bo[args.buf].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp'
+
+    local opts = { buffer = args.buf, noremap = true, silent = true }
+
+    opts.desc = 'Open float for diagnostics under cursor'
+    vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
+
+    opts.desc = 'Goto declaration of symbol'
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+
+    opts.desc = 'Goto implementation of symbol'
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+
+    opts.desc = 'Goto symbol type definition'
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+
+    opts.desc = 'Reformat buffer'
+    vim.keymap.set('n', '<leader>f', function()
+      vim.lsp.buf.format({ async=true })
+    end, opts)
+  end,
+})
