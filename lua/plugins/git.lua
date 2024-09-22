186137f9e8b07bf95ff84505d6e085f05b89538c
Devon Gardner <devon@goosur.com>
2024-09-21 22:42:23 -0400

    Add git-blame.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,3 +1,7 @@
+require('mini.git').setup({})
+
 MiniDeps.add('lewis6991/gitsigns.nvim')
 require('gitsigns').setup({})
-require('mini.git').setup({})
+
+MiniDeps.add('f-person/git-blame.nvim')
+require('gitblame').setup({})
