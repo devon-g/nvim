c5434a18d3bbbbd4ad7313c95cbc903afe402757
Devon Gardner <devon@goosur.com>
2024-09-21 21:57:59 -0400

    Updated option preferences

--- lua/options.lua
+++ lua/options.lua
@@ -12,10 +12,7 @@ vim.o.smartcase = true
 -- Appearance
 vim.o.colorcolumn = '80'
 vim.o.number = true
-vim.o.termguicolors = true
 
 -- Behavior
 vim.o.completeopt = 'menuone,noinsert,preview,noselect'
-vim.o.splitbelow = true
-vim.o.splitright = true
 vim.o.undofile = true
