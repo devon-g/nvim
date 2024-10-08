f017cb45a130afdf32acdffa85449c715145140c
Devon Gardner <devon@goosur.com>
2024-10-08 13:49:53 -0400

    Customize blink.cmp keymaps

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -8,6 +8,10 @@ return {
     },
     version = "v0.*",
     opts = {
+      keymap = {
+        select_prev = { "<C-p>" },
+        select_next = { "<C-n>" },
+      },
       highlight = { use_nvim_cmp_as_default = true, },
       accept = { auto_brackets = { enabled = true } },
       trigger = { signature_help = { enabled = true } },
