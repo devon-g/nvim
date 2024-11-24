336e3a27b65d5c0eddefa6895bef3db28c826f7d
Devon Gardner <devon@goosur.com>
2024-11-24 11:57:19 -0500

    switched colorscheme from gruvbox to catpuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
+    "catppuccin/nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.api.nvim_command("colorscheme gruvbox")
+      vim.api.nvim_command("colorscheme catppuccin")
     end,
   },
   "nvim-tree/nvim-web-devicons",
