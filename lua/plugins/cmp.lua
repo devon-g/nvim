78d5a8b6e6c107fad9c78c1e3493208ba1cf69ce
Devon Gardner <devon@goosur.com>
2024-12-11 20:48:29 -0500

    Clean up nvim-cmp config using opts not config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,43 +1,44 @@
 return {
   "hrsh7th/nvim-cmp",
-  lazy = false,
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
   },
-  config = function()
+  opts = function(_, opts)
     local cmp = require("cmp");
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          vim.snippet.expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "vsnip" },
-      }, {
-        { name = "buffer" },
-      }),
+
+    -- Use neovim builtin snippets
+    if not opts.snippet then
+      opts.snippet = {}
+    end
+
+    opts.snippet.expand = function(args)
+      vim.snippet.expand(args.body)
+    end
+
+    -- Configure mappings
+    if not opts.mapping then
+      opts.mapping = {}
+    end
+
+    opts.mapping = cmp.mapping.preset.insert({
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }),
     })
 
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        { name = "cmdline" },
-      }),
-      matching = { disallow_symbol_nonprefix_matching = false },
+    -- Completion sources
+    if not opts.sources then
+      opts.sources = {}
+    end
+
+    opts.sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "vsnip" },
+    }, {
+      { name = "buffer" },
     })
   end,
 }
