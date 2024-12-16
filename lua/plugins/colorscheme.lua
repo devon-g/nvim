2b6d841455545d36a733c56fb28be215a3b9ef13
Devon Gardner <devon@goosur.com>
2024-12-15 20:52:03 -0500

    can't stay away from gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,8 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.api.nvim_command("colorscheme catppuccin")
-    end,
-  },
-  "nvim-tree/nvim-web-devicons",
+  "ellisonleao/gruvbox.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.api.nvim_command("colorscheme gruvbox")
+  end,
 }
