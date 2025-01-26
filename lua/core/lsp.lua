23726f2512e035b2aebd2ddbbbb50028915ef725
Devon Gardner <devon@goosur.com>
2025-01-25 21:31:36 -0500

    added lsps and blink

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,8 +1,10 @@
 -- Default config
-vim.lsp.config('*', {
-  root_markers = { '.git' },
+vim.lsp.config("*", {
+  root_markers = { ".git" },
 })
 
 -- Enable servers configured in lsp/<server_name>.lua
-vim.lsp.enable('lua_ls')
-vim.lsp.enable('superhtml')
+vim.lsp.enable("clangd")
+vim.lsp.enable("css_lsp")
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("superhtml")
