c162cde13a4c64bbcc23f160749e66d720806a07
Devon Gardner <devon@goosur.com>
2025-11-03 00:14:49 -0500

    reformatted and set ivy default profile

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,15 +1,17 @@
 return {
   {
     "ibhagwan/fzf-lua",
-    dependencies = {
-      { "junegunn/fzf", build = "./install --bin" },
+    opts = {
+      { "ivy", "hide" }, -- profile selection
     },
-    opts = {},
-    config = function()
+    config = function(_, opts)
       local fzf_lua = require("fzf-lua")
+      fzf_lua.setup(opts)
 
-      vim.keymap.set("n", "<leader>ff", fzf_lua.files, { desc = "Fuzzy find files" })
-      vim.keymap.set("n", "<leader>fg", fzf_lua.live_grep, { desc = "Fuzzy find string" })
+      vim.keymap.set("n", "<leader>ff", fzf_lua.files,
+        { desc = "Fuzzy find files" })
+      vim.keymap.set("n", "<leader>fg", fzf_lua.live_grep,
+        { desc = "Fuzzy find string" })
     end,
   },
 }
